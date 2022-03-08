62b2b52d73bcefb1d0b769d4cd1510b766a79251
kimxwan <kimxwan0319@naver.com>
2022-03-08 14:53:08 +0900

    init

---
+++ AROUNDTALK/AROUNDTALK/AROUNDTALKApp.swift
@@ -0,0 +1,17 @@
+//
+//  AROUNDTALKApp.swift
+//  AROUNDTALK
+//
+//  Created by 김수완 on 2022/03/08.
+//
+
+import SwiftUI
+
+@main
+struct AROUNDTALKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
